1e6c3bd4182751b1b6e85606b8f88433a5a14e80
PaulG09 <lotuds107@gmail.com>
2023-12-20 21:30:27 +0000

    Do hard things

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  * _abs - Computes the absolute value of an integer.
+ *   * int : The integer to be computed.
+ *    *
+ *     * Return: The absolute value of the integer.
+ */
+int _abs(int)
+{
+	return ((n >= 0) ? n : -n);
+}
