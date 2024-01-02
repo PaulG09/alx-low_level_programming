f68dded91878d3fa96324e2148f81fc9dbff9c1d
PaulG09 <lotuds107@gmail.com>
2024-01-02 23:37:49 +0000

    Do hard things

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - check the code
+ * n - input character to be assigned value
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	if (n != NULL)
+	{
+		*n = 98;
+	}
+
+	return (0);
+}
