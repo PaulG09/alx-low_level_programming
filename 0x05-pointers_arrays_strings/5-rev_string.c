186ce964877ad06b5956cf72e7a73f4bdf1526bc
PaulG09 <lotuds107@gmail.com>
2024-01-03 16:44:03 +0000

    Do hard things

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * rev_string - check code
+ * @s: input parameter
+ * Return: Always s
+ */
+void rev_string(char *s)
+{
+	if (s != NULL)
+	{
+		int length = 0;
+
+		while (s[length] != '\0')
+		{
+			length++;
+		}
+
+		int start = 0;
+		int end = length - 1;
+
+		while (start < end)
+		{
+			char temp = s[start];
+
+			s[start] = s[end];
+			s[end] = temp;
+			start++;
+			end--;
+		}
+	}
+}
