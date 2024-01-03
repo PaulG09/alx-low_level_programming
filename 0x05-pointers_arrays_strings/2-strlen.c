761b2206b3f29aa438ceab7b284d88f2593c1a90
PaulG09 <lotuds107@gmail.com>
2024-01-03 15:26:30 +0000

    Do hard things

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - check code
+ * @s: input character
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int len = _strlen(s);
+
+	_putchar(len);
+
+	return (0);
+}
