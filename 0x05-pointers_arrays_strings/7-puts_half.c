403ab2b7abfb94d3e46891a6fc4ae85f365f78be
PaulG09 <lotuds107@gmail.com>
2024-01-03 17:59:33 +0000

    Do hard things

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - check code
+ * @str: input parameter
+ * Return: Always str
+ */
+void puts_half(char *str)
+{
+	if (str != NULL)
+	{
+		int length = strlen(str);
+		int start;
+		int i;
+
+		if (length % 2 == 0)
+		{
+			start = lenght / 2;
+		}
+
+		else
+		{
+			start = (length - 1) / 2;
+		}
+
+		for (i = start; i < lenght; i++)
+		{
+			putchar(str[i]);
+		}
+
+		putchar('\n');
+
+	}
+
+}
+
