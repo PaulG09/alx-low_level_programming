0495a62670cd2592fc31934b998837458c72b9f4
PaulG09 <lotuds107@gmail.com>
2024-01-08 18:08:53 +0000

    Do hard things

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - check code
+ * @s: input parameter
+ * @c: input parameter
+ * Return: Always s(success);
+ */
+char *_strchr(char *s, char c)
+{
+	if (s != '\0')
+	{
+		while (*s == c)
+		{
+			return (s);
+		}
+
+		s++;
+	}
+
+	else
+	{
+		return (NULL);
+	}
+}
