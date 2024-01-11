8afa0d072b1a49e5418c48d2a2575e21c7542df1
PaulG09 <lotuds107@gmail.com>
2024-01-11 21:19:26 +0000

    Do hard things

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _sqrt_recursion - check code
+ *  @n: input parameter
+ *  Return: n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n != n * n / n)
+	{
+		return (-1);
+	}
+
+	return (n * _sqrt_recursion(n - 1))
+}
