663574455f1b215bbdfae531be87510cb6f4bb37
PaulG09 <lotuds107@gmail.com>
2024-01-11 20:12:52 +0000

    Do hard things

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,10 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
+		return;
 	}
 
-	putchar(*s);
-	putchar(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
