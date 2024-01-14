16771fb6b68ef63c26b89f532eb1acd4aaf06459
PaulG09 <lotuds107@gmail.com>
2024-01-14 20:53:51 +0000

    Do hard things

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  main - check code
+ *  agrc:input parameter
+ *  agrv:input parameter
+ *  Return: Always 0
+ */
+int main(int agrc, char *agrv[])
+{
+	printf("%d\n", agrc);
+
+	return (0);
+}
