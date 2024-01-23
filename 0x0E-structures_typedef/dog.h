c88bb60e73018574fe2799317ebb8e0d7ed06279
PaulG09 <lotuds107@gmail.com>
2024-01-23 10:50:34 +0000

    Do hard things

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog{
-	
+/**
+ * struct dog - d efinition of a new type
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: declares and defines a datablock of struct dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
