e379103ad31affff936d963b1a8b14189e788838
PaulG09 <lotuds107@gmail.com>
2024-01-23 10:07:54 +0000

    Do hard things

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file it was compiled from, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	reutrn (0);
+}
