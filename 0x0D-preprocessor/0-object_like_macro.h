a631ff4c917ad293eec5ba1a5db5428e6258bd38
PaulG09 <lotuds107@gmail.com>
2024-01-23 09:46:45 +0000

    Do hard things

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+/**
+ * OBJECT_LIKE_MACRO_H - defines a macro named SIZE as an abbreviation for the token 1024
+ * REturn: Always  OBJECT_LIKE_MACRO
+ */
+
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+ C - Preprocessor: macros, cosntants and directives
