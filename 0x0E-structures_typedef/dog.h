f0011e86eae63d2634f7eed2586d6d96dd217e5a
PaulG09 <lotuds107@gmail.com>
2024-01-23 10:55:09 +0000

    Do hard things

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - d efinition of a new type
+ * struct dog - definition of a new type
  * @name: first member
  * @age: second member
  * @owner: third member
@@ -13,5 +13,5 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 #endif /* DOG_H */
