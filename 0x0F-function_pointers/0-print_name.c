8ea4dde13b35fa7db7014c6ab191f907ce5cba1a
PaulG09 <lotuds107@gmail.com>
2024-01-26 17:51:45 +0000

    Do hard things

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: input string
+ * @f: pointer to function
+ * Return: Always name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (*name != NULL && *f != NULL)
+	{
+		f(name);
+	}
+}
