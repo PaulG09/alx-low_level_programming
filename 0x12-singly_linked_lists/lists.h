bb574de98161ac2071c1bad25e314a1b696d1d97
PaulG09 <lotuds107@gmail.com>
2024-02-01 19:16:19 +0000

    Do harrd things

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -10,6 +10,7 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  */
 typedef struct list_s
@@ -20,8 +21,6 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-int _putchar(char c);
-
 
 
 #endif /* LISTS_H  */
