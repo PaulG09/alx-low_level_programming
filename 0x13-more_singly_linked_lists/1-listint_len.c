2f41a2af383c0bc254f6e318e5e1c3e30e9420c7
PaulG09 <lotuds107@gmail.com>
2024-02-05 19:03:22 +0000

    Do hard things

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * listint_len - eturns the number of elements in a linked listint_t list.
+ * @h: input parameter
+ * Return: lists number
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t list;
+	int s;
+
+	list = 0;
+	s = strlen(h);
+	while (h != NULL)
+	{
+		return ();
+		h = h->next;
+		h++
+	}
+	return (s);
+
+}
